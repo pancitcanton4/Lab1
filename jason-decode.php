8fa712eb94742841e1398606617fac65fa5431d5
ej <ejtan1997@gmail.com>
2019-01-14 23:53:13 -0700

    json-decode

---
+++ jason-decode.php
@@ -0,0 +1,19 @@
+<?php
+
+function w3rfunction($value,$key)
+{
+    echo "$key : $value"."<br>";
+}
+$a = '{
+"Title": "The Cuckoos Calling",
+"Author": "Robert Galbraith",
+"Detail":
+      {
+      "Publisher": "Little Brown"
+      }
+
+
+}';
+$j1 = json_decode($a,true);
+array_walk_recursive($j1,"w3rfunction");
+?>
